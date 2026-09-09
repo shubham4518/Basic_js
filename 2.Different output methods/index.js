60f19a220d563ba92837eacb5058826c23e2b757
Shubham Chaudhary <shubhamchaudhary7084@gmail.com>
2026-09-09 16:13:08 +0530

    js basics

---
+++ 2.Different output methods/index.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+console.info("This is an informational message.");
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+alert("This is an alert message.");
+confirm("Do you want to proceed?");
+document.write("This is a message written to the document.");
+document.writeln("This is a message written to the document with a new line.");
---
+++ 3.var let const/index.js
@@ -0,0 +1,89 @@
+//Declaration of variables using var
+// var a;
+// console.log(a); //undefined
+
+// //Initialization of variable using var
+// var b ;
+// b=10;
+// console.log(b); //10
+
+// //declarationn and initialization of variable using var
+// var c = 20;
+// console.log(c); //20
+
+// //re-initialization of variable using var
+// var c;
+// c='Hello';
+// c='cat';
+// console.log(c); //cat
+
+// //re-declaration and re-initialisation  
+// var d= 10;
+// var d= 20;
+// console.log(d); //20
+
+// //hoisting of variable using var
+// console.log(e);
+// var e = 30;
+
+
+// now for let 
+
+// //Declaration of variables using let
+// let a;
+// console.log(a); //undefined
+
+// //Initialization of variable using var
+// let b ;
+// b=10;
+// console.log(b); //10
+
+// //declarationn and initialization of variable using let
+// let C= 20;
+// console.log(C); //20
+
+// //re-initialization of variable using let
+// let c;
+// c='Hello';
+// c='cat';
+// console.log(c); //cat
+
+// //re-declaration and re-initialisation   let
+// let d= 10;
+// let d= 20;
+// console.log(d); //20
+
+// //hoisting of variable using var
+// console.log(e);
+// var e = 30;
+
+
+// const 
+
+//Declaration of variables using const
+// const a;
+// console.log(a); //undefined
+
+//Initialization of variable using const
+// const b ;
+// b=10;
+// console.log(b); //10
+
+//declarationn and initialization of variable using const
+// const c = 20;
+// console.log(c); //20
+
+//re-initialization of variable using const
+// const c;
+// c='Hello';
+// c='cat';
+// console.log(c); //cat
+
+//re-declaration and re-initialisation  
+// const d= 10;
+// const d= 20;
+// console.log(d); //20
+
+//hoisting of variable using const
+console.log(e);
+const e = 30;
