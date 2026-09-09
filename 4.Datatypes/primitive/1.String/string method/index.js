01b1219a624856dacda787e763cdf04bcacf3f3c
Shubham Chaudhary <shubhamchaudhary7084@gmail.com>
2026-09-09 16:54:35 +0530

    string methods

---
+++ 4.Datatypes/primitive/1.String/string method/index.js
@@ -0,0 +1,48 @@
+var a="Hello World "
+console.log(a)
+
+var b= a.length
+console.log(b)
+
+var c= a.charAt(4)
+console.log(c)
+
+var d= a.charCodeAt(4)
+console.log(d)
+
+var e=a.toUpperCase()
+console.log(e)
+
+var f=a.toLowerCase()
+console.log(f)
+
+var g =a.startsWith('Hello')
+console.log(g)
+
+var h= a.endsWith(' ')
+console.log(h)
+
+var i = a.includes('World')
+console.log(i)
+
+var j = a.indexOf("o")
+console.log(j)
+
+var k = " Besant tech  "
+
+var l = k.trimStart()
+console.log(l)
+
+var m =k.trimEnd()
+console.log(m)
+
+var n=k.trim()
+console.log(n)
+
+var o=k.lastIndexOf('e')
+console.log(o)
+
+var p = a.repeat(3)
+console.log(p)
+
+
