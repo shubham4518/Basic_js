571c927a9a6de3a3fc35766177ef75ce3e8197c6
Shubham Chaudhary <shubhamchaudhary7084@gmail.com>
2026-09-09 16:29:06 +0530

    using interpolation

--- 4.Datatypes/primitive/1.String/string main/index.js
+++ 4.Datatypes/primitive/1.String/string main/index.js
@@ -1,5 +1,12 @@
-var a='I am shubham';
+var a='I am "shubham" ';
 console.log(a);
 
 var b=" I am 'Shubham' ";
 console.log(b);
+
+var c= ` I am 'Shubham ' "Chaudhary"`
+console.log(c);
+
+var name="Shubham";
+var place ="Uttar pradesh";
+console.log(` My name is ${name} and i am from ${place} `);
