79dbe11dbeee0679f1a0b0fdce9b9d5b6e782222
Shubham Chaudhary <shubhamchaudhary7084@gmail.com>
2026-09-13 18:54:31 +0530

    good

--- 4.Datatypes/primitive/5.Number/Number methods/index.js
+++ 4.Datatypes/primitive/5.Number/Number methods/index.js
@@ -1,3 +1,21 @@
 let a= 134.567
+console.log(a , typeof a)
 
 let b= a.toString()
+console.log(b , typeof b)
+
+let c = a.toFixed(2)
+console.log(c , typeof c)
+
+let d= 100000
+let e = d.toLocaleString()
+console.log(e ,typeof e)
+
+let f= 3 * "Hello"
+console.log(f , typeof f)
+
+console.log (isNaN(f))
+console.log (isFinite(Infinity))
+
+let g= +"33"
+console.log(g , typeof g)
--- 4.Datatypes/primitive/5.Number/index.js
+++ 4.Datatypes/primitive/5.Number/index.js
@@ -1,2 +1,11 @@
 let a=10
 console.log(a , typeof a)
+
+let b=135.567
+console.log(b , typeof b)
+
+let c= Infinity
+console.log(c, typeof c)
+
+let d = -Infinity
+console.log(d, typeof d)
